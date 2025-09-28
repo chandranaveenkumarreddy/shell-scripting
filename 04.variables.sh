e450ad65a00aea4badd1de7920f4d70bfe1f42b2
chandranaveenkumarreddy <chandranaveenkumarreddy@gmail.com>
2025-09-28 15:21:19 +0530

    variables with arguments

---
+++ 04.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2 #the key value is caps and no space between before and after equal
+
+echo "$PERSON1: hi $PERSON2"
+echo "$PERSON2:: hi $PERSON1"
+echo "$PERSON1:: how are you"
+echo "$PERSON2:: i'm good, how about you?"
+echo "$PERSON1: good"
